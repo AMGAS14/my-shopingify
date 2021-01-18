320083e59af9a94b2934e54887d7411b1b35c5e9
Antonio Antunes <amgas14@gmail.com>
2021-01-18 19:42:07 +0000

    input module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms' ;
-import { MatInputModel } from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations' ;
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModel
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
