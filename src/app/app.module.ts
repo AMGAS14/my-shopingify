6147b5755fb5e0d5aedc40322574fc3e24f58c67
Antonio Antunes <amgas14@gmail.com>
2021-01-19 19:46:36 +0000

    toolbar commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { FormsModule } from '@angular/forms' ;
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card' ;
 import { MatButtonModule } from '@angular/material/button' ;
-
+import { MatToolbarModule } from '@angular/material/toolbar' ;
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent} from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations' ;
-
+import { HeaderComponent } from "./header/header.component" ;
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +25,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations' ;
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
